8fa04ae86b2b0e680850c5bce9fef86c7399ec0d
Saad Arfan <82287213+MelonBun8@users.noreply.github.com>
2025-07-15 03:23:45 +0500

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'https://autoline-backend.onrender.com',
     'http://localhost:3500',
     "http://localhost:5173", // Vite usually runs frontend on port 5173
     'https://www.autolineshowroom.com', // fake URL
